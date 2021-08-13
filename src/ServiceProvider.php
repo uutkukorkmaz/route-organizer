5df851320160a40cdec1509cf27b08b5c3d43cc6
uutkukorkmaz <uutkukorkmaz>
2021-08-13 19:54:38 +0300

    stubs updated

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,25 +4,27 @@
 namespace Uutkukorkmaz\RouteOrganizer;
 
 
-use Uutkukorkmaz\RouteOrganizer\Console\Commands\MakeRouteGroupCommand;
+
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
 
     /**
      * Register the service provider
-     * 
+     *
      * @return void
     */
     public function register()
     {
-//        $this->app->singleton('command.routeorganizer.make',fn($app) => new MakeRouteGroupCommand);
     }
-    
+
     public function boot(){
         $this->publishes([
-            __DIR__.'/Console/Commands/MakeRouteGroupCommand.php' => app_path('Console/Commands/MakeRouteGroupCommand.php')
+            __DIR__.'/Console/Commands/MakeRouteGroupCommand.php' => app_path('Console/Commands/MakeRouteGroupCommand.php'),
+            __DIR__.'../stubs/route.empty.stub' => app_path('../stubs/route.empty.stub'),
+            __DIR__.'../stubs/route.group-only.stub' => app_path('../stubs/route.group-only.stub'),
+            __DIR__.'../stubs/route.resource.stub' => app_path('../stubs/route.resource.stub'),
         ],'routeorganizer');
     }
-    
+
 }
