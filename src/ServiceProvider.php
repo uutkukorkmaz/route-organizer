7cdea1b07ca9ee695ef8a0b418cfa7e7ab344788
uutkukorkmaz <uutkukorkmaz>
2021-08-13 19:57:21 +0300

    paths fixed

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -21,9 +21,9 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     public function boot(){
         $this->publishes([
             __DIR__.'/Console/Commands/MakeRouteGroupCommand.php' => app_path('Console/Commands/MakeRouteGroupCommand.php'),
-            __DIR__.'../stubs/route.empty.stub' => app_path('../stubs/route.empty.stub'),
-            __DIR__.'../stubs/route.group-only.stub' => app_path('../stubs/route.group-only.stub'),
-            __DIR__.'../stubs/route.resource.stub' => app_path('../stubs/route.resource.stub'),
+            __DIR__.'/../stubs/route.empty.stub' => app_path('../stubs/route.empty.stub'),
+            __DIR__.'/../stubs/route.group-only.stub' => app_path('../stubs/route.group-only.stub'),
+            __DIR__.'/../stubs/route.resource.stub' => app_path('../stubs/route.resource.stub'),
         ],'routeorganizer');
     }
 
