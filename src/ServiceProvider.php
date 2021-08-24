416e8ec1f805488649785d8dd75ea00813babd6d
uutkukorkmaz <uutkukorkmaz>
2021-08-24 23:26:09 +0300

    paths and stubs fixed

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,27 +4,16 @@
 namespace Uutkukorkmaz\RouteOrganizer;
 
 
-
-
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
-
-    /**
-     * Register the service provider
-     *
-     * @return void
-    */
-    public function register()
+    public function boot()
     {
-    }
-
-    public function boot(){
         $this->publishes([
-            __DIR__.'/Console/Commands/MakeRouteGroupCommand.php' => app_path('Console/Commands/MakeRouteGroupCommand.php'),
-            __DIR__.'/../stubs/route.empty.stub' => base_path('stubs/route.empty.stub'),
-            __DIR__.'/../stubs/route.group-only.stub' => base_path('stubs/route.group-only.stub'),
-            __DIR__.'/../stubs/route.resource.stub' => base_path('stubs/route.resource.stub'),
-        ],'route-organizer');
+            __DIR__ . '/Console/Commands/MakeRouteGroupCommand.php' => app_path('Console/Commands/MakeRouteGroupCommand.php'),
+            __DIR__ . '/../stubs/route.empty.stub' => base_path('stubs/route.empty.stub'),
+            __DIR__ . '/../stubs/route.group-only.stub' => base_path('stubs/route.group-only.stub'),
+            __DIR__ . '/../stubs/route.resource.stub' => base_path('stubs/route.resource.stub'),
+        ], 'route-organizer');
     }
 
 }
